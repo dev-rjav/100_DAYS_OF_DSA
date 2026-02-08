67b57c260e49428603ecea1c1e59aa1c9850066c
dev-rjav <dev.rjav@outlook.com>
2026-02-08 23:58:36 +0530

    Completed Day8

---
+++ Day8_b.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0)
+        return false;
+    return (n & (n - 1)) == 0;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (isPowerOfTwo(n))
+        printf("true\n");
+    else
+        printf("false\n");
+
+    return 0;
+}
