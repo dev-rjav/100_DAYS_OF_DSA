1a659eda32564b3755635693787f524f76ff3c91
dev-rjav <dev.rjav@outlook.com>
2026-02-16 09:02:11 +0530

    Completed Day 15

--- Day15_b.c
+++ Day15_b.c
@@ -1,41 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+int main(){
+     int n, m;
+     printf("Enter the number of rows:");
+     scanf("%d",&n);
+     printf("Enter the number of columns:");
+     scanf("%d",&m);
 
-int main() {
-    int m, n;
-    printf("Enter the number of rows: ");
-    scanf("%d", &m);
-    printf("Enter the number of columns: ");
-    scanf("%d", &n);
-    
-    int matrix[m][n];
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("Enter the element at position (%d,%d): ", i+1, j+1);
-            scanf("%d", &matrix[i][j]);
+     int matrix[n][m];
+     for(int i = 0;i<n;i++){
+         for(int j=0;j<m;j++){
+            printf("Enter the element at index %d,%d: ",i,j);
+             scanf("%d",&matrix[i][j]);
         }
-    }
-
-    int row, col;
-    printf("Enter the row and column to set to zero: ");
-    scanf("%d %d", &row, &col);
-    
-    for(int i = 0; i < m; i++) {
-        if(i == row) {
-            for(int j = 0; j < n; j++) {
-                if(j == col) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
-    
-    printf("\nThe matrix is:\n");
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    
+     }
     return 0;
 }
